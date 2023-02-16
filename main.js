7dc61a1649e4d9e1f04105a1495c2c2fa925dcc8
Melanie Forbes <mforbes12@gmail.com>
2023-02-16 13:21:55 -0300

    background color change on hover

--- main.js
+++ main.js
@@ -6,11 +6,20 @@ test.forEach((text) => {
   text.style.color = 'red';
 })
 
-
+let colorFlow = ["#FDDDE6","#FDD5DF", "#FECDD7", "#FEC6D0", "#FFBEC8", "#FFB6C1"]
 //changes background color to pink as you mouse over
 const divs = document.querySelectorAll('div')
 divs.forEach((div) => {
-  div.addEventListener('mouseover', function() {
-    div.style.backgroundColor = 'pink'
-  })
-})
+  div.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = colorFlow[0];
+
+    setTimeout(() => {
+      event.target.style.backgroundColor = colorFlow[1];
+      event.target.style.backgroundColor = colorFlow[2];
+      event.target.style.backgroundColor = colorFlow[3];
+      event.target.style.backgroundColor = colorFlow[4];
+      event.target.style.backgroundColor = colorFlow[5];
+    }, 1000);
+
+  }, false);
+});
