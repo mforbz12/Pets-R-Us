f789f1bcec17941594c3a812280e964f866efde7
Melanie Forbes <mforbes12@gmail.com>
2023-02-16 14:05:24 -0300

    updated popup with images and gradient change

--- main.js
+++ main.js
@@ -6,20 +6,32 @@ test.forEach((text) => {
   text.style.color = 'red';
 })
 
-let colorFlow = ["#FDDDE6","#FDD5DF", "#FECDD7", "#FEC6D0", "#FFBEC8", "#FFB6C1"]
+
+let colorFlow = ["#fff1f5","#FDDDE6","#FDD5DF", "#FECDD7", "#FEC6D0", "#FFBEC8", "#FFB6C1"]
 //changes background color to pink as you mouse over
 const divs = document.querySelectorAll('div')
-divs.forEach((div) => {
-  div.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = colorFlow[0];
 
-    setTimeout(() => {
-      event.target.style.backgroundColor = colorFlow[1];
-      event.target.style.backgroundColor = colorFlow[2];
-      event.target.style.backgroundColor = colorFlow[3];
-      event.target.style.backgroundColor = colorFlow[4];
-      event.target.style.backgroundColor = colorFlow[5];
-    }, 1000);
+divs.forEach((div) => {
+  setTimeout(() => {
+      div.style.backgroundColor = colorFlow[0];
+    }, 6000);
+  setTimeout(() => {
+    div.style.backgroundColor = colorFlow[1];
+  }, 7000);
+  setTimeout(() => {
+    div.style.backgroundColor = colorFlow[2];
+  }, 8000);
+  setTimeout(() => {
+    div.style.backgroundColor = colorFlow[3];
+  }, 9000);
+  setTimeout(() => {
+    div.style.backgroundColor = colorFlow[4];
+  }, 10000);
+  setTimeout(() => {
+    div.style.backgroundColor = colorFlow[5];
+  }, 11000);
+  setTimeout(() => {
+    div.style.backgroundColor = colorFlow[6];
+  }, 12000);
 
   }, false);
-});
