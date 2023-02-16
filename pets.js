9c1be1b98968669b7517c62f9397de83835146d6
Melanie Forbes <mforbes12@gmail.com>
2023-02-16 14:13:30 -0300

    updated with emily's changes

---
+++ pets.js
@@ -0,0 +1,21 @@
+const root = document.getElementById('container');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+
+    const photoDiv = document.getElementById('dog-photos')
+    const getDogPhotos = async () =>{
+        const photos = await fetch("https://dog.ceo/api/breeds/image/random/5")
+        .then(res => res.json())
+        .then(data =>{
+            data.message.forEach((element, index) => {
+                const newImage = document.createElement('img')
+                newImage.id = `photo${index}`
+                newImage.src = element
+
+                photoDiv.appendChild(newImage)
+                console.log(data.message)
+            });
+        }).catch(err => console.log('error in fetch: ', err))
+    }
+    getDogPhotos();
+})
